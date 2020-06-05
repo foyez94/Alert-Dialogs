40ecd18c5c7c28a1e84f184138882644e4c40be5
foyez <foyez@foyezs-MacBook-Pro.local>
2020-06-04 22:04:05 -0400

    first commit

---
+++ app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.alertdialog;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AlertDialog.Builder alertDialog;
+    private Button showDialog;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showDialog =(Button) findViewById(R.id.showButtonId);
+        showDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Show the actual dialog(alert dialog)
+                alertDialog = new AlertDialog.Builder(MainActivity.this);
+
+                //set things up = setup tittle
+                //alertDialog.setTitle(R.String.title);
+                alertDialog.setTitle(getResources().getString(R.string.title));
+
+                //SetMessage
+                alertDialog.setMessage(getResources().getString(R.string.Message));
+
+                //Set  Cancelable
+                alertDialog.setCancelable(false);
+                //set possitive Button;
+                alertDialog.setPositiveButton(getResources().getString(R.string.yes), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Exit our window Activity
+                        MainActivity.this.finish();
+                    }
+                });
+                //set negative button
+
+                alertDialog.setNegativeButton(getResources().getString(R.string.no), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                       dialog.cancel();
+                    }
+                });
+
+                //create the actual dialog
+
+                AlertDialog dialog = alertDialog.create();
+
+                //Show the dialog
+                dialog.show();
+
+
+
+
+            }
+        });
+
+    }
+
+}
